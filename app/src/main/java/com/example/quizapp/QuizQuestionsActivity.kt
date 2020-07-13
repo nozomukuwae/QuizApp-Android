d0a355704b0edc0b5313d9482719a28fd2f532fc
Nozomu Kuwae <nozomukuwae@gmail.com>
2020-07-13 20:04:47 +0900

    Show correct option

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -7,14 +7,20 @@ import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
+import org.w3c.dom.Text
 import java.lang.IllegalArgumentException
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    enum class SubmitMode {
+        NOT_YET, DONE
+    }
+
     private val questionFactory = QuestionFactory()
     private var questionList: Array<Question>? = null
     private var currentIndex = 0
-    private var selectedOptionIndex: Int? = null
+    private var selectedOptionView: TextView? = null
     private var score = 0
+    private var submitMode = SubmitMode.NOT_YET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,14 +55,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View?) {
         if (view !is TextView) return
         highlightOptions(view)
-
-        selectedOptionIndex = when(view?.id) {
-            option1.id -> 0
-            option2.id -> 1
-            option3.id -> 2
-            option4.id -> 3
-            else -> throw IllegalArgumentException("$view is unknown")
-        }
+        selectedOptionView = view
     }
 
     private fun highlightOptions(selectedOption: TextView) {
@@ -82,17 +81,57 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     fun onSubmit(view: View) {
         val questionListVal = questionList ?: return
+        val selectedOptionViewVal = selectedOptionView ?: return
 
-        if (selectedOptionIndex == questionListVal[currentIndex].answerIndex) {
-            score ++
+        when(submitMode) {
+            SubmitMode.NOT_YET -> {
+                if (getOptionIndex(selectedOptionViewVal) == questionListVal[currentIndex].answerIndex) {
+                    score ++
+                    selectedOptionViewVal.background = ContextCompat.getDrawable(this, R.drawable.correct_option_border_bg)
+                } else {
+                    selectedOptionViewVal.background = ContextCompat.getDrawable(this, R.drawable.wrong_option_border_bg)
+                    val correctOptionView = getOptionView(questionListVal[currentIndex].answerIndex)
+                    correctOptionView.background = ContextCompat.getDrawable(this, R.drawable.correct_option_border_bg)
+                }
+
+                submitMode = SubmitMode.DONE
+                if (currentIndex < questionListVal.size - 1) {
+                    submit_button.text = getString(R.string.go_to_next_question_button)
+                } else {
+                    submit_button.text = getString(R.string.show_result_button)
+                }
+            }
+            SubmitMode.DONE -> {
+                if (currentIndex < questionListVal.size - 1) {
+                    currentIndex ++
+                } else {
+                    currentIndex = 0
+                }
+
+                submitMode = SubmitMode.NOT_YET
+                submit_button.text = getString(R.string.submit_button)
+                showQuestion()
+            }
         }
+    }
 
-        if (currentIndex < questionListVal.size - 1) {
-            currentIndex ++
-            showQuestion()
-        } else {
-            currentIndex = 0
-            showQuestion()
+    private fun getOptionIndex(view: View): Int {
+        return when(view.id) {
+            option1.id -> 0
+            option2.id -> 1
+            option3.id -> 2
+            option4.id -> 3
+            else -> throw IllegalArgumentException("$view is unknown")
+        }
+    }
+
+    private fun getOptionView(index: Int): TextView {
+        return when(index) {
+            0 -> option1
+            1 -> option2
+            2 -> option3
+            3 -> option4
+            else -> throw IllegalArgumentException("$index is unknown")
         }
     }
 }
